f9ed88a0cfea00fc09c2be99e20511e54eb44dbc
wangxingkang <156148958@qq.com>
2020-09-16 22:25:28 +0800

    feat: 添加预览入口

--- .umirc.ts
+++ .umirc.ts
@@ -2,13 +2,17 @@ export default {
   mode: 'site',
   title: 'react-admin',
   description: 'A magical react admin',
-  favicon: './logo.svg',
-  logo: './logo.svg',
+  favicon: 'https://react-admin-site.vercel.app/logo.svg',
+  logo: 'https://react-admin-site.vercel.app/logo.svg',
   resolve: {
     includes: ['src']
   },
   navs: [
     null,
+    {
+      title: '预览',
+      path: 'https://react-admin-plus.vercel.app'
+    },
     {
       title: 'GitHub',
       path: 'https://github.com/pansyjs/react-admin'
